7da4db87e8fb21750b02edc5651979f353d95edb
tronxi <tronxi97@gmail.com>
2018-07-30 20:20:58 +0200

    enviar imagen al servidor

--- camaraRaspberry.py
+++ camaraRaspberry.py
@@ -2,6 +2,10 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import cv2
+import base64
+
+from urllib.parse import urlencode 
+from urllib.request import Request, urlopen
 
 camera = PiCamera()
 camera.resolution = (640, 480)
@@ -13,6 +17,21 @@ time.sleep(0.1)
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     
     image = frame.array
+    #
+    codificado_correctamente, buffer = cv2.imencode('.png', image)
+	if codificado_correctamente:
+        imagen_en_base64 = base64.b64encode(buffer).decode('utf-8')
+
+		url = 'http://192.168.0.5/camara-web-web/php/subirImagen.php'
+		datos_enviar = {'imagens': imagen_en_base64}
+
+		print("Enviando foto...")
+
+		peticion = Request(url, urlencode(datos_enviar).encode())
+		respuesta = urlopen(peticion).read().decode()
+		
+		print("Guardada con éxito")
+    #
     cv2.imshow("Image", image)
 
     key = cv2.waitKey(1) & 0xFF
