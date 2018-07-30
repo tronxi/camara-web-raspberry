22b39ed9268a08a4820f7850b0844596e1fb3149
tronxi <tronxi97@gmail.com>
2018-07-30 20:27:32 +0200

    enviar imagen servidor

--- camaraRaspberry.py
+++ camaraRaspberry.py
@@ -17,21 +17,17 @@ time.sleep(0.1)
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     
     image = frame.array
-    #
     codificado_correctamente, buffer = cv2.imencode('.png', image)
-	if codificado_correctamente:
+
+    if codificado_correctamente:
         imagen_en_base64 = base64.b64encode(buffer).decode('utf-8')
+        url = 'http://192.168.0.5/camara-web-web/php/subirImagen.php'
+        datos_enviar = {'imagens': imagen_en_base64}
+        print("Enviando foto...")
+        peticion = Request(url, urlencode(datos_enviar).encode())
+        respuesta = urlopen(peticion).read().decode()
+        print("Guardada con éxito")
 
-		url = 'http://192.168.0.5/camara-web-web/php/subirImagen.php'
-		datos_enviar = {'imagens': imagen_en_base64}
-
-		print("Enviando foto...")
-
-		peticion = Request(url, urlencode(datos_enviar).encode())
-		respuesta = urlopen(peticion).read().decode()
-		
-		print("Guardada con éxito")
-    #
     cv2.imshow("Image", image)
 
     key = cv2.waitKey(1) & 0xFF
