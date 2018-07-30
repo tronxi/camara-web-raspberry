18b3816625f12f70b4f3efa942410cc11ee78e16
tronxi <tronxi97@gmail.com>
2018-07-30 17:53:11 +0200

    cambio resolucion

--- camaraRaspberry.py
+++ camaraRaspberry.py
@@ -4,9 +4,9 @@ import time
 import cv2
 
 camera = PiCamera()
-camera.resolution = (2592, 1944)
+camera.resolution = (640, 480)
 camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(2592, 1944))
+rawCapture = PiRGBArray(camera, size=(640, 480))
  
 time.sleep(0.1)
 
