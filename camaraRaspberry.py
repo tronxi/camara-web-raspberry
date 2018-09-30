b0f5bbd97d6739ef87b89e18bf139fdf9bcbbc45
Sergio Garcia Sanchez <sergio.gsanchez@alumnos.upm.es>
2018-09-30 20:55:02 +0200

    sin deteccion de ojos

--- camaraRaspberry.py
+++ camaraRaspberry.py
@@ -45,13 +45,7 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     gray = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for(x,y,w,h) in faces:
-        detectada = True
         imagen = cv2.rectangle(imagen, (x,y), (x+w, y+h), (255,0,0),2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = imagen[y:y+h, x:x+w]
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-        for (ex,ey,ew,eh) in eyes:
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
     enviarImagen(imagen)
 
 
