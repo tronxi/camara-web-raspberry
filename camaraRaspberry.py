00c99094856db25e0da7ecebd4b1c032d06ab7fb
Sergio Garcia Sanchez <sergio.gsanchez@alumnos.upm.es>
2018-10-02 15:22:40 +0200

    variable detectado

--- camaraRaspberry.py
+++ camaraRaspberry.py
@@ -36,14 +36,14 @@ def buscarCaras(imagen):
     gray = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for(x,y,w,h) in faces:
-        detectado = True
+        this.detectado = True
         imagen = cv2.rectangle(imagen, (x,y), (x+w, y+h), (255,0,0),2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = imagen[y:y+h, x:x+w]
         eyes = eye_cascade.detectMultiScale(roi_gray)
         for (ex,ey,ew,eh) in eyes:
             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-    print(detectado)
+    print(this.detectado)
     return imagen
 
 signal.signal(signal.SIGINT, signal_handler)
