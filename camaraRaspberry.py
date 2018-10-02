a6750f011c779c43a6fa0c67fd9bc48b32c23cd6
Sergio Garcia Sanchez <sergio.gsanchez@alumnos.upm.es>
2018-10-02 15:20:37 +0200

    variable detectado

--- camaraRaspberry.py
+++ camaraRaspberry.py
@@ -12,6 +12,7 @@ from urllib.request import Request, urlopen
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+detectado = False
 def signal_handler(sig, frame):  
         enviarImagen(cv2.imread("error.jpg"))
         time.sleep(1)
@@ -30,16 +31,19 @@ def enviarImagen(imagen):
         rawCapture.truncate(0)
     else:
         print("error al codificar")
+
 def buscarCaras(imagen):
     gray = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for(x,y,w,h) in faces:
+        detectado = True
         imagen = cv2.rectangle(imagen, (x,y), (x+w, y+h), (255,0,0),2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = imagen[y:y+h, x:x+w]
         eyes = eye_cascade.detectMultiScale(roi_gray)
         for (ex,ey,ew,eh) in eyes:
             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+    print(detectado)
     return imagen
 
 signal.signal(signal.SIGINT, signal_handler)
